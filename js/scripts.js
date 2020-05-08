d32bbfe73905210cff290d3a41970bdf7d6ac1e3
me <me@me.me.com>
2020-05-08 13:51:10 -0700

    implement spec 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 function encryptSentence(sentence){
- var length = sentence.split("").filter(function(item){
-   return /[a-z]/i.test(item)
+ var length = sentence.split("").filter(function(character){
+  /[a-z]/i.test(character);
  }).length;
+
+ var words = sentence.split(" ").map(function (word){
+  return word.split("").filter(function(character){
+    return /[a-z]/i.test(character);
+  });
+});
+console.log(length);
+console.log(words);
 }
 
 
+
 $(document).ready(function(){
-  
   encryptSentence("don't compare yourself to others, compare yourself to the person you were yesterday")
 });
