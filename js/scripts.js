40b8f1950c078d46b5c9d71bdc6f7998d1cdd169
me <me@me.me.com>
2020-05-08 15:16:46 -0700

    implement the fifth spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,55 @@
 function encryptSentence(sentence){
  
- var wordsFlat = sentence.split("").filter(function(character){
-  return /[a-z]/i.test(character);
- });
+  var wordsFlat = sentence.split("").filter(function(character){
+    return /[a-z]/i.test(character);
+  });
 
- var length = wordsFlat.length;
-
-
-
-var column = 8;
-var row = 9;
+  var length = wordsFlat.length;
 
 
-//create the square
-var array2d = [];
-for(var i = 1; i <= row; i++) {
-   if (wordsFlat.length < column){
-     var length = wordsFlat.length
-     array2d.push(wordsFlat.splice(0, wordsFlat.length).concat(Array(column - length).fill(" ")));
-   }
-   else{
-   array2d.push(wordsFlat.splice(0,column));
-   }
-}
-
-//flatten the square
-var new1dArray = [];
-for(var j = 0; j < column; j++){
-  for(var i = 0; i < row; i++){
-    new1dArray.push(array2d[i][j]); 
+  var column = 8;
+  var row = 9;
+  //create the square
+  var array2d = [];
+  for(var i = 1; i <= row; i++) {
+    if (wordsFlat.length < column){
+      var length = wordsFlat.length
+      array2d.push(wordsFlat.splice(0, wordsFlat.length).concat(Array(column - length).fill(" ")));
+    }
+    else{
+    array2d.push(wordsFlat.splice(0,column));
+    }
   }
-}
 
+  //flatten the square
+  var new1dArray = [];
+  for(var j = 0; j < column; j++){
+    for(var i = 0; i < row; i++){
+      new1dArray.push(array2d[i][j]); 
+    }
+  }
 
-console.log(array2d);
-console.log(length);
-console.log(new1dArray);
+  var removeSpaces = new1dArray.filter(item => !(item === " "));
+ 
+
+  finalArray = []
+  while(removeSpaces.length > 0){
+    if(removeSpaces.length < 5){
+      finalArray.push(removeSpaces.splice(0,removeSpaces.length).join(""));
+    }
+    else{
+      finalArray.push(removeSpaces.splice(0,5).join(""));
+    }
+  }
+
+  var finalSentence = finalArray.join(" ");
+  
+  console.log(finalSentence)
+  console.log(finalArray)
+  console.log(removeSpaces);
+  console.log(array2d);
+  console.log(length);
+  console.log(new1dArray);
 }
 
 
