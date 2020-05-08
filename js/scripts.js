be9ea59960118d3841491085077bfd76a0d68d5d
me <me@me.me.com>
2020-05-08 13:41:47 -0700

    implement the first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+function encryptSentence(sentence){
+ var length = sentence.split("").filter(function(item){
+   return /[a-z]/i.test(item)
+ }).length;
+}
+
+
 $(document).ready(function(){
   
+  encryptSentence("don't compare yourself to others, compare yourself to the person you were yesterday")
 });
